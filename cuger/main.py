fea442c1ea2c322a90b9de3036de06b1fa0a0959
Romeo-Leeyh <1905516054@qq.com>
2026-01-04 16:27:16 +0800

    v1.3.0

--- cuger/main.py
+++ cuger/main.py
@@ -1,7 +1,4 @@
 import os, sys, time
-
-from __transform.convexify import MoosasConvexify
-from __transform.graph import MoosasGraph
 import __transform.process as ps
 
 main_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -19,9 +16,9 @@ output = r"E:/DATA/CUGER_energy_data/"
 
 def process_file(input_geo_path, modelname):
     paths = ps.get_output_paths(modelname, output)
-    if os.path.exists(paths["new_xml_path"]):
+    if os.path.exists(paths["new_idf_path"]):
         print(f"--Skip-- | {modelname}")
-        
+        #return
     print(f"Processing file: {input_geo_path}, basename: {modelname}")
     
     try:
@@ -36,29 +33,19 @@ def process_file(input_geo_path, modelname):
 
         Moosas.saveModel(model, paths["new_geo_path"], save_type="geo")
         Moosas.saveModel(model, paths["new_xml_path"], save_type="xml")
-        Moosas.saveModel(model, paths["new_rdf_path"], save_type="rdf")
-        Moosas.saveModel(model, paths["new_idf_path"], save_type="idf")
+
 
         ps.graph_process(paths["new_geo_path"], paths["new_xml_path"], paths["output_json_path"], paths["figure_graph_path"])
+
+        Moosas.saveModel(model, paths["new_rdf_path"], save_type="rdf")
+        Moosas.saveModel(model, paths["new_idf_path"], save_type="idf")
+        
     except ValueError as e:
         print(f"ValueError: {e} - Modelname: {modelname}")
     except FileNotFoundError as e:
         print(f"FileNotFoundError: {e} - Modelname: {modelname}")
     except Exception as e:
         print(f"Unexpected error: {e} - Modelname: {modelname}")
-    
-    """
-    ps.convex_process(input_geo_path, paths["output_geo_path"], paths["figure_convex_path"])
-    Moosas.transform(paths["output_geo_path"],
-                    solve_contains=False, 
-                    divided_zones=True, 
-                    break_wall_horizontal=True, 
-                    solve_redundant=True,
-                    attach_shading=False,
-                    standardize=True)
-
-    ps.graph_process(paths["new_geo_path"], paths["new_xml_path"], paths["output_json_path"], paths["figure_graph_path"])
-    """
   
 
 for dirpath, dirnames, filenames in os.walk(input):
